5fcc6c12ac71c496dc59702d6cfdedec46031a00
Grace Kuo <gkuo.github@gmail.com>
2020-10-18 21:07:49 -0600

    added console logs for all score

---
+++ highscore.js
@@ -0,0 +1,7 @@
+var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+var highScorePrint = document.querySelector(".scoresHTML");
+var clearHighScore = document.getElementById("clearScores");
+
+console.log(highscores);
+console.log(highScorePrint);
+console.log(clearHighScore);
